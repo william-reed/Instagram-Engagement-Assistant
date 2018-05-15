d0fdb296c0a2368ced4eb54d0e628296e51fa459
William Reed <wreed58@gmail.com>
2018-05-14 21:46:26 -0400

    added relationships

--- models.py
+++ models.py
@@ -8,11 +8,14 @@ class Scan(Base):
 	__tablename__ = 'scan'
 
 	scan_id = Column(BigInteger, primary_key=True, autoincrement=True)
-	# same as user_pk from instagram api
+	# same as user_pk from instagram api; the user being scanned
 	instagram_user_id = Column(BigInteger, ForeignKey("instagram_user.instagram_user_id"))
 	timestamp = Column(DateTime, default=datetime.datetime.utcnow)
 	# the client user that initiated this scan
 	initiated_by = Column(Integer, ForeignKey("instagram_user.instagram_user_id"))
+
+	scanned_user = relationship("Instagram_User", back_populates="scanned_by")
+	initiating_user = relationship("Instagram_User", back_populates="initiated_scans")
 
 # an instagram account
 class Instagram_User(Base):
@@ -26,6 +29,12 @@ class Instagram_User(Base):
 	is_business = Column(Boolean, nullable=False)
 	is_private = Column(Boolean, nullable=False)
 
+	scanned_by = relationship("Scan", back_populates="scanned_user")
+	initiated_scans = relationship("Scan", back_populates="initiating_user")
+	media = relationship("Media", back_populates="instagram_user")
+	comments = relatinoship("Comment", back_populates="instagram_user")
+
+
 # an instagram media entity (photo, video, etc)
 class Media(Base):
 	__tablename__ = 'media'
@@ -33,6 +42,10 @@ class Media(Base):
 	media_id = Column(BigInteger, primary_key=True)
 	instagram_user_id = Column(BigInteger, ForeignKey("instagram_user.instagram_user_id"), nullable=False)
 	is_picture = Column(Boolean)
+
+	instagram_user = relationship("Instagram_User", back_populates="media")
+	comments = relationship("Comment", back_populates="media")
+
 
 # a users comment
 class Comment(Base):
@@ -43,12 +56,20 @@ class Comment(Base):
 	instagram_user_id = Column(BigInteger, ForeignKey("instagram_user.instagram_user_id"), nullable=False)
 	text = Column(String(length=4096), nullable=False)
 
+	instagram_user = relationship("Instagram_User", back_populates="comments")
+	media = relationship("Media", back_populates="comments")
+
 # a non automated user engagement
 class Interaction(Base):
 	__tablename__ = 'interaction'
 
 	interaction_id = Column(Integer, primary_key=True, autoincrement=True)
+	media_id = Column(BigInteger, ForeignKey("media.media_id"), nullable=False)
 	# the account interacted with
 	interacted_id = Column(BigInteger, ForeignKey("instagram_user.instagram_user_id"), nullable=False)
 	# the account that created the interaction
 	interactor_id = Column(BigInteger, ForeignKey("instagram_user.instagram_user_id"), nullable=False)
+
+	media = relationship("Media")
+	interacted = relationship("Instagram_User")
+	interactor = relationship("Instagram_User")
