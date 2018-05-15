9ca8d691f04f48f7191ac687bb8942a13f61a8cb
William Reed <wreed58@gmail.com>
2018-05-14 20:01:36 -0400

    fixed spelling

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 mysql_user = os.environ['MYSQL_USER']
 mysql_pass = os.environ['MYSQL_PASS']
 mysql_url = os.environ['MYSQL_URL']
-db_name = os.environ['ENGAGAMENT_DB']
+db_name = os.environ['ENGAGEMENT_DB']
 
 db_conneciton = 'mysql://' + mysql_user + ':' + mysql_pass + '@' + mysql_url + '/' + livethread_db
 
