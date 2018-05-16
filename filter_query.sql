64d822f47bedcd1ee295d1be13ca83c95028d46a
William Reed <wreed58@gmail.com>
2018-05-15 23:13:33 -0400

    added type query

--- filter_query.sql
+++ filter_query.sql
@@ -1,6 +1,6 @@
 SELECT username, count(*) 
 	FROM instagram_user
 	JOIN comment USING (instagram_user_id)
-	WHERE followers < 750 AND following < 750
+	WHERE followers < 750 AND following < 750 AND type = 0
 	GROUP BY username
 	ORDER BY count(*) asc;
