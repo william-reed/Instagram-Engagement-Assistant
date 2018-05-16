5e6411d0da4a215bee02fce788bf5d2ae6adeb0a
William Reed <wreed58@gmail.com>
2018-05-15 20:05:41 -0400

    fetch_media and added checks for pre-existing data

--- scraper.py
+++ scraper.py
@@ -1,7 +1,9 @@
 from sqlalchemy imoprt create_engine
 from sqlalchemy.orm import sessionmaker
 
-from InstagramApi import InstagramApi
+from InstagramAPI import InstagramAPI
+
+from models import Scan, Instagram_User, Media, Comment, Interaction
 
 import logging
 import sys
@@ -38,30 +40,41 @@ if __name__ == "__main__":
 	logger.debug("Tables created / exist")
 
 	# setup instagram
-	InstagramApi = InstagramAPI(config.INSTA_USER, config.INSTA_PASS)
-	InstagramApi.login()
+	api = InstagramAPI(config.INSTA_USER, config.INSTA_PASS)
+	api.login()
 
 	###########################################################################
 	## Processing
 	###########################################################################
 	init_scan()
-	user_pks = fetch_users(users, InstagramApi)
+	user_pks = fetch_users(users, api)
+
 
 
 
 def init_scan():
 
-def fetch_users(usernames, api):
+def fetch_users(usernames, api, session, force_update=False):
 	"""
 	Fetch the user objects from the api for the given usernames and insert them into the DB
 	:usernames The list of usernames to look at
 	:api The instagram API
+	:session the db session
+	:force_update if true the queries will overide previous user entries in the db to update them
 	:returns list of pk's of the given usernames.
 	"""
 	pks = []
 	for username in usernames:
 		api.getInfoByName(username.strip())
 		user = api.LastJson["user"];
+		user_pk = user["pk"]
+
+		# does the DB contain the user_pk?
+		# would demorgans make this more readable?
+		if session.query(Instagram_User).get(user_pk) != None and not force_update:
+			# skip this instance since they are already in the db and we are not updating them
+			continue
+
 		instagram_user = Instagram_User(instagram_user_id = user["pk"],
 		 username=user["username"],
 		 followers=user['follower_count'],
@@ -76,5 +89,39 @@ def fetch_users(usernames, api):
 		time.sleep(config.SLEEP_TIME)
 
 	Logger.debug("Gatered users committed to database")
+	session.commit()
+	return pks
+
+def fetch_media(user_pks, api, session, force_update=False):
+	"""
+	Fetch the media for these user pk's and isnert them into the DB
+	:user_pks a list of instagram user primary keys
+	:api The isntagram API
+	:session the dbsession
+	:force_update if true the queries will overide previous user entries in the db to update them
+	:returns a list of media pk's
+	"""
+	pks = []
+	for user_pk in user_pks:
+		api.getUserFeed(user_pk)
+		media = api.LastJson["items"]
+
+		# is medium the singular? probably not...
+		for medium in media:
+			media_pk = medium["pk"]
+
+			# make sure pk not in db
+			if session.query(Media).get(media_pk) != None and not force_update:
+				continue
+
+			pks.append(media_pk)
+			is_picture = medium['media_type'] == 1 ? True : False
+
+			instagram_media = Media(media_id=media_pk,
+				instagram_user_id=user_pk,
+				is_picture=is_picture)
+
+		session.commit()
+		
 	session.commit()
 	return pks
